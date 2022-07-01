26fe2f2f11843596838dcb7e0548b13a5a04a15c
thomasvrussell <dearthomasrussell@icloud.com>
2022-07-01 22:23:45 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ MAINTAINER_EMAIL = "hulei@pmo.ac.cn"
 DOWNLOAD_URL = 'https://github.com/thomasvrussell/sfft'
 
 LICENSE = 'MIT Licence'
-VERSION = '1.1.2'
+VERSION = '1.1.3'
 
 install_reqs = ['scipy>=1.5.2',
                 'astropy>=3.2.3',
