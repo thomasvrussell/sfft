3b38152562a9b0eeb6801255a346c8cc68612e13
Lauren Aldoroty <laurenaldoroty@gmail.com>
2025-06-12 11:57:50 -0700

    bugfix

--- sfft/SpaceSFFTCupyFlow.py
+++ sfft/SpaceSFFTCupyFlow.py
@@ -368,7 +368,7 @@ class SpaceSFFT_CupyFlow:
     def create_variance_image( self ):
 
         assert self.PixA_targetVar_GPU.flags['C_CONTIGUOUS']
-        assert self.self.PixA_resamp_objectVar_GPU.flags['C_CONTIGUOUS']
+        assert self.PixA_resamp_objectVar_GPU.flags['C_CONTIGUOUS']
 
         # calculate variance image for (un-decorrelated) difference image
         NX, NY = self.PixA_target_GPU.shape
@@ -377,7 +377,7 @@ class SpaceSFFT_CupyFlow:
 
         # Note: convolve a squared kernel on the variance image to get the variance of the convolved image
         # Note: let's skip the matching kernel here, as it is expected to be a minor compensation.
-        FPixA_DIFFVar_GPU = cp.fft.fft2(self.self.PixA_resamp_objectVar_GPU) * cp.fft.fft2(PSF_target_CSZ_GPU ** 2) + \
+        FPixA_DIFFVar_GPU = cp.fft.fft2(self.PixA_resamp_objectVar_GPU) * cp.fft.fft2(PSF_target_CSZ_GPU ** 2) + \
             cp.fft.fft2(self.PixA_targetVar_GPU) * cp.fft.fft2(PSF_object_CSZ_GPU ** 2)
         
         FPixA_dDIFFVar_GPU = FPixA_DIFFVar_GPU * cp.fft.fft2(cp.fft.ifft2(self.FKDECO_GPU) ** 2)
